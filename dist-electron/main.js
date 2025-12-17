3c9558c0b7b6250f9f31aa90d4ea521724e2d2f5
don4ara <don4araa@gmail.com>
2025-12-17 19:52:38 +0500

    feat: Add splash screen, improve single instance handling, and simplify build output path.

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,57 +1,84 @@
-import { app as n, BrowserWindow as i } from "electron";
-import { fileURLToPath as c } from "node:url";
-import o from "node:path";
-const s = o.dirname(c(import.meta.url));
-process.env.APP_ROOT = o.join(s, "..");
-const r = process.env.VITE_DEV_SERVER_URL, R = o.join(process.env.APP_ROOT, "dist-electron"), l = o.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = r ? o.join(process.env.APP_ROOT, "public") : l;
-let e, t;
-function a() {
-  t = new i({
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
+process.env.APP_ROOT = path.join(__dirname$1, "..");
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
+let win;
+let splash;
+function createWindow() {
+  splash = new BrowserWindow({
     width: 400,
     height: 400,
-    transparent: !1,
-    frame: !1,
-    alwaysOnTop: !0,
+    transparent: false,
+    frame: false,
+    alwaysOnTop: true,
     backgroundColor: "#242424",
     webPreferences: {
-      nodeIntegration: !1
+      nodeIntegration: false
     }
-  }), t.loadFile(o.join(s, "../electron/splash.html")), e = new i({
+  });
+  splash.loadFile(path.join(__dirname$1, "../electron/splash.html"));
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
     minWidth: 800,
     minHeight: 600,
-    icon: o.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
-    show: !1,
-    frame: !1,
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    show: false,
+    frame: false,
     // Frameless window
     titleBarStyle: "hidden",
     // Native controls on macOS
     backgroundColor: "#242424",
     webPreferences: {
-      preload: o.join(s, "preload.mjs")
+      preload: path.join(__dirname$1, "preload.mjs")
     }
-  }), e.once("ready-to-show", () => {
+  });
+  win.once("ready-to-show", () => {
     setTimeout(() => {
-      t == null || t.close(), t = null, e == null || e.show();
+      splash == null ? void 0 : splash.close();
+      splash = null;
+      win == null ? void 0 : win.show();
     }, 1500);
-  }), e.webContents.on("did-finish-load", () => {
-    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  }), r ? e.loadURL(r) : e.loadFile(o.join(l, "index.html"));
+  });
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
 }
-n.on("window-all-closed", () => {
-  process.platform !== "darwin" && (n.quit(), e = null);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
 });
-n.on("activate", () => {
-  i.getAllWindows().length === 0 && a();
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
-const d = n.requestSingleInstanceLock();
-d ? (n.on("second-instance", (m, f, p) => {
-  e && (e.isMinimized() && e.restore(), e.focus());
-}), n.whenReady().then(a)) : n.quit();
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on("second-instance", (_event, _commandLine, _workingDirectory) => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
+      win.focus();
+    }
+  });
+  app.whenReady().then(createWindow);
+}
 export {
-  R as MAIN_DIST,
-  l as RENDERER_DIST,
-  r as VITE_DEV_SERVER_URL
+  MAIN_DIST,
+  RENDERER_DIST,
+  VITE_DEV_SERVER_URL
 };
